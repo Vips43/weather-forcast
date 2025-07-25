ec2093a2a03747fd3596f833ccb4bb18f3410f8d
Vipul Gehlot <vipulgehlot43@gmail.com>
2025-07-25 12:40:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+let input = document.querySelector('#input').value
+let search =document.querySelector('#search')
+let cityName = document.getElementById('city-name')
+let weatherStatus = document.getElementById('status')
+let API_key = '901be245c2974afa304b1285ac063b38'
+
+search.addEventListener('click', ()=>{
+
+  let cityName = input.value
+  console.log(cityName)
+  
+})
+
+async function processData() {
+  const getData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=901be245c2974afa304b1285ac063b38`);
+    console.log('Data received:', getData);
+    let jsonData = await getData.json()
+    console.log(jsonData);
+    console.log(jsonData.name)
+    cityName.innerText = jsonData.name
+    weatherStatus.innerText = jsonData.weather[0].main
+    document.getElementById('wind').innerText = jsonData.wind.speed
+    document.getElementById('rain').innerText = jsonData.weather[0].description
+    document.getElementById('humidity').innerText = jsonData.main.humidity + "%"
+    document.getElementById('temp').innerText = Math.floor((jsonData.main.temp)/10) + "°C"
+    document.getElementById('rise').innerText = jsonData.sys.sunrise
+    document.getElementById('set').innerText = jsonData.sys.sunset
+    
+}
+processData()
