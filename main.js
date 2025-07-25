bb50290a1d05494126ffec9ed74c9f7f724fdf48
Vips43 <vipulgehlot43@gmail.com>
2025-07-25 14:13:09 +0530

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let input = document.querySelector('#input').value
+let input = document.querySelector('#input')
 let search =document.querySelector('#search')
 let cityName = document.getElementById('city-name')
 let weatherStatus = document.getElementById('status')
@@ -6,18 +6,20 @@ let API_key = '901be245c2974afa304b1285ac063b38'
 
 search.addEventListener('click', ()=>{
 
-  let cityName = input.value
-  console.log(cityName)
-  
-})
-
-async function processData() {
+      let cityName = input.value
+      console.log(cityName)
+      processData(cityName)
+    })
+    
+async function processData(input) {
   const getData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=901be245c2974afa304b1285ac063b38`);
+  
     console.log('Data received:', getData);
+    
     let jsonData = await getData.json()
     console.log(jsonData);
     console.log(jsonData.name)
-    cityName.innerText = jsonData.name
+    cityName.innerText = jsonData.name;
     weatherStatus.innerText = jsonData.weather[0].main
     document.getElementById('wind').innerText = jsonData.wind.speed
     document.getElementById('rain').innerText = jsonData.weather[0].description
@@ -27,4 +29,3 @@ async function processData() {
     document.getElementById('set').innerText = jsonData.sys.sunset
     
 }
-processData()
