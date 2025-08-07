35979d4f93a181c1bf47ee97ab24c506c15f8025
Vips43 <vipulgehlot43@gmail.com>
2025-08-07 22:24:52 +0530

    new update

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ let image = document.getElementById("image");
 let notFound = document.getElementById("not-found");
 let main = document.getElementById("main");
 let weatherResult = document.querySelector(".result");
+let loader = document.querySelector(".loader");
 
 
 let API_key = "901be245c2974afa304b1285ac063b38";
@@ -20,7 +21,7 @@ searchBtn.addEventListener("click", () => {
   else {
     let cityName = input.value.trim();
     processData(cityName);
-    
+
   }
 });
 
@@ -28,17 +29,18 @@ async function processData(city) {
   const getData = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=901be245c2974afa304b1285ac063b38`
   );
-  
+
   let jsonData = await getData.json();
-  
+
   if (getData.status == "404") {
     weatherResult.classList.add("none");
     notFound.classList.remove("none");
     searchImg.classList.add('none')
-    
+    main.style.background = 'white'
+
   } else {
     const windSpeed = jsonData.wind.speed * 3.6;
-    
+
     searchImg.classList.add('none')
     notFound.classList.add("none");
     weatherResult.classList.remove("none");
@@ -50,45 +52,62 @@ async function processData(city) {
       jsonData.main.humidity + "%";
     document.getElementById("temp").innerText =
       Math.floor(jsonData.main.temp - 273.15) + "°C";
-    
+
     const sunrise = new Date(jsonData.sys.sunrise * 1000);
     const sunset = new Date(jsonData.sys.sunset * 1000);
-    
+
     document.getElementById(
       "rise"
     ).innerText = `sun rise on :- ${sunrise.toLocaleTimeString()}`;
     document.getElementById(
       "set"
     ).innerText = `sun sets on :- ${sunset.toLocaleTimeString()}`;
-    
     weatherInfo(jsonData);
   }
-  
+
 
   //real time weather description update
-  function weatherInfo(jsonData){
-    if (jsonData.weather[0].description == "overcast clouds" || jsonData.weather[0].description == "scattered clouds" || jsonData.weather[0].main == "Clouds"){
-      cloudStatus.classList.add("fa-solid","fa-cloud","text-3xl",'rotate');
-      image.src = "cloudy.png";
+  function weatherInfo(jsonData) {
+    if (jsonData.weather[0].description == "overcast clouds" || jsonData.weather[0].description == "scattered clouds" || jsonData.weather[0].main == "Clouds") {
+      cloudStatus.classList.add("fa-solid", "fa-cloud", "text-3xl", 'rotate');
+      loader.style.display = 'block'
+      setTimeout(() => {
+        image.src = "cloudy.png";
+        loader.style.display = 'none'
+      }, 1000)
+
       image.classList.add('shadow')
       main.style.background = "rgb(161, 159, 159)"
     }
-    else if (jsonData.weather[0].description == "rain" || jsonData.weather[0].description == "light rain" || jsonData.weather[0].description == "heavy rain" || jsonData.weather[0].description == "heavy intensity rain" || jsonData.weather[0].description == "moderate rain"){
+    else if (jsonData.weather[0].description == "rain" || jsonData.weather[0].description == "light rain" || jsonData.weather[0].description == "heavy rain" || jsonData.weather[0].description == "heavy intensity rain" || jsonData.weather[0].description == "moderate rain") {
 
-      
+      loader.style.display = 'block'
+      setTimeout(() => {
         image.src = "rain.png";
-        cloudStatus.classList.add("fa-solid","fa-cloud-rain","text-3xl",'rotate', "text-3xl");
-        image.classList.add('shadow')
-        main.style.background = "linear-gradient(to top,rgb(24, 80,90),rgb(21, 119, 103),rgb(57, 170, 204))"
-      }
-      else if (jsonData.weather[0].description == "clear sky" || jsonData.weather[0].description == "broken clouds" || jsonData.weather[0].description == "clear") {
-          cloudStatus.classList.add("fa-solid", "fa-sun", "text-3xl");
-          image.src = "clears.png";
-          image.classList.add('sun-shadow')
-          main.style.background = "linear-gradient(to bottom, white, lightblue, skyblue)"
+        loader.style.display = 'none'
+      }, 1000)
+      cloudStatus.classList.add("fa-solid", "fa-cloud-rain", "text-3xl", 'rotate', "text-3xl");
+      image.classList.add('shadow')
+      main.style.background = "linear-gradient(to top,rgb(24, 80,90),rgb(21, 119, 103),rgb(57, 170, 204))"
+    }
+    else if (jsonData.weather[0].description == "clear sky" || jsonData.weather[0].description == "broken clouds" || jsonData.weather[0].description == "clear") {
+
+      cloudStatus.classList.add("fa-solid", "fa-sun", "text-3xl");
+      loader.style.display = 'block'
+      setTimeout(() => {
+        image.src = "clears.png";
+        loader.style.display = 'none'
+      }, 1000)
+      image.classList.add('sun-shadow')
+      main.style.background = "linear-gradient(to bottom, white, lightblue, skyblue)"
     }
     else if (jsonData.weather[0].description == "storm") {
-      image.src = "strom.png";
+
+      loader.style.display = 'block'
+      setTimeout(() => {
+        image.src = "strom.png";
+        loader.style.display = 'none'
+      }, 1000)
       image.classList.add('shadow')
       cloudStatus.classList.add("fa-solid", "fa-cloud-bolt", "text-3xl");
       main.style.background = "rgb(82, 81, 81);"
