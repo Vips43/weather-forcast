fc162387a205c2bdd6564b2e983455c6bf66981c
Vips43 <vipulgehlot43@gmail.com>
2025-08-18 15:55:41 +0530

    de

--- main.js
+++ main.js
@@ -20,7 +20,9 @@ searchBtn.addEventListener("click", () => {
   }
   else {
     let cityName = input.value.trim();
-    processData(cityName);
+    setTimeout(()=>{
+      processData(cityName);
+    },2000)
     main.style.color = 'black'
 
   }
@@ -41,6 +43,7 @@ async function processData(city) {
     main.style.background = 'white'
 
   } else {
+
     const windSpeed = jsonData.wind.speed * 3.6;
 
     searchImg.classList.add('none')
@@ -74,60 +77,29 @@ async function processData(city) {
   function weatherInfo(jsonData) {
     if (jsonData.weather[0].description == "overcast clouds" || jsonData.weather[0].description == "scattered clouds" || jsonData.weather[0].main == "Clouds") {
       cloudStatus.classList.add("fa-solid", "fa-cloud", "text-3xl", 'rotate');
-      loader.style.display = 'block'
-      setTimeout(() => {
-        image.src = "cloudy.png";
-        loader.style.display = 'none'
-      }, 1000)
-
+      image.src = "cloudy.png";
       image.classList.add('shadow')
       main.style.backgroundImage = "linear-gradient(to bottom, #483E3E, #585858, #848A9B, #BABCCD)"
       main.style.color = 'white'
     }
     else if (jsonData.weather[0].description == "rain" || jsonData.weather[0].description == "light rain" || jsonData.weather[0].description == "heavy rain" || jsonData.weather[0].description == "heavy intensity rain" || jsonData.weather[0].description == "moderate rain") {
 
-
       image.src = "rain.png";
       cloudStatus.classList.add("fa-solid", "fa-cloud-rain", "text-3xl", 'rotate', "text-3xl");
       image.classList.add('shadow')
       main.style.background = "linear-gradient(to top,rgb(24, 80,90),rgb(21, 119, 103),rgb(57, 170, 204))"
-
     }
     else if (jsonData.weather[0].description == "clear sky" || jsonData.weather[0].description == "broken clouds" || jsonData.weather[0].description == "clear") {
+
       cloudStatus.classList.add("fa-solid", "fa-sun", "text-3xl");
+
       image.src = "clears.png";
-      image.classList.add('sun-shadow')
-      main.style.background = "linear-gradient(to bottom, white, lightblue, skyblue)"
-    }
-    else if (jsonData.weather[0].description == "rain" || jsonData.weather[0].description == "light rain" || jsonData.weather[0].description == "heavy rain" || jsonData.weather[0].description == "heavy intensity rain" || jsonData.weather[0].description == "moderate rain") {
 
-      loader.style.display = 'block'
-      setTimeout(() => {
-        image.src = "rain.png";
-        loader.style.display = 'none'
-      }, 1000)
-      cloudStatus.classList.add("fa-solid", "fa-cloud-rain", "text-3xl", 'rotate', "text-3xl");
-      image.classList.add('shadow')
-      main.style.background = "linear-gradient(to top,rgb(24, 80,90),rgb(21, 119, 103),rgb(57, 170, 204))"
-    }
-    else if (jsonData.weather[0].description == "clear sky" || jsonData.weather[0].description == "broken clouds" || jsonData.weather[0].description == "clear") {
-
-      cloudStatus.classList.add("fa-solid", "fa-sun", "text-3xl");
-      loader.style.display = 'block'
-      setTimeout(() => {
-        image.src = "clears.png";
-        loader.style.display = 'none'
-      }, 1000)
       image.classList.add('sun-shadow')
       main.style.background = "linear-gradient(to bottom, white, lightblue, skyblue)"
     }
     else if (jsonData.weather[0].description == "storm") {
-
-      loader.style.display = 'block'
-      setTimeout(() => {
-        image.src = "strom.png";
-        loader.style.display = 'none'
-      }, 1000)
+      image.src = "strom.png";
       image.classList.add('shadow')
       cloudStatus.classList.add("fa-solid", "fa-cloud-bolt", "text-3xl");
       main.style.background = "rgb(82, 81, 81);"
@@ -142,8 +114,7 @@ function reset() {
   cloudStatus.className = "";
   notFound.classList.add('none')
   searchImg.classList.remove('none')
-  main.style.background = "#B2B0E8"
+  main.style.background = "#9699BE"
   main.style.color = 'black'
 }
 
-// updated on 12:52
